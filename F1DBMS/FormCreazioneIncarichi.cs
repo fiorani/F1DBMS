3afec2d8557e12a4558c804d1b52daac3b33bd94
Mattia Panni <mattia.panni@studio.unibo.it>
2022-06-21 01:51:26 +0200

    Renamed file

---
+++ F1DBMS/FormCreazioneIncarichi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F1DBMS
+{
+    public partial class FormCreazioneIncarichi : Form
+    {
+        private readonly DataClassesF1DataContext db;
+        private int rigaTabellaDipendente = -1;
+        private int rigaTabellaTeam = -1;
+
+        public FormCreazioneIncarichi(DataClassesF1DataContext originDb)
+        {
+            InitializeComponent();
+            db = originDb;
+            SelezionaDipPerIncarico.DataSource = db.dipendentis;
+            SelezionaTeamPerInc.DataSource = db.teams;
+        }
+
+        private void SottoscriviIncaricoBtn_Click(object sender, EventArgs e)
+        {
+            if(rigaTabellaDipendente >= 0 && rigaTabellaTeam >= 0 && !StipendioBox.Text.Equals(String.Empty) && RuoloComboBox.SelectedItem != null)
+            {
+                incarichi_dipendenti newIncarico = new incarichi_dipendenti();
+                newIncarico.CF = SelezionaDipPerIncarico.Rows[rigaTabellaDipendente].Cells["CF"].Value.ToString();
+                newIncarico.IDTeam = SelezionaTeamPerInc.Rows[rigaTabellaTeam].Cells["IDTeam"].Value.ToString();
+                newIncarico.dataAssunzione = dataInizioIncarico.Value;
+                newIncarico.Ruolo = RuoloComboBox.SelectedItem.ToString();
+                newIncarico.stipendio = StipendioBox.Text;
+                newIncarico.dataLicenziamento = null;
+                try
+                {
+                    /* mancano i controlli LINQ sull'overlap di date */
+                    db.incarichi_dipendentis.InsertOnSubmit(newIncarico);
+                    db.SubmitChanges();
+                    MessageBox.Show("Incarico sottoscritto!");
+                    this.Close();
+                } catch (Exception)
+                {
+                    MessageBox.Show("Ricontrolla Campi!");
+                }
+            } else
+            {
+                MessageBox.Show("Ricontrolla Campi!");
+            }
+        }
+
+        private void SelezionaDipPerIncarico_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            rigaTabellaDipendente = e.RowIndex;
+        }
+
+        private void SelezionaTeamPerInc_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            rigaTabellaTeam = e.RowIndex;
+        }
+    }
+}
