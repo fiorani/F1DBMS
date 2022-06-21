529ac8e7dac318205e09e79dbecfd49a627877ab
Mattia Panni <mattia.panni@studio.unibo.it>
2022-06-21 12:53:07 +0200

    Add form revocaIncaricoPiloti

--- F1DBMS/FormRevocaIncarichiPiloti.Designer.cs
+++ F1DBMS/FormRevocaIncarichiPiloti.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace F1DBMS
 {
-    partial class Form1
+    partial class FormRevocaIncarichiPiloti
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,82 @@ namespace F1DBMS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.revocaIncPilBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DataLicenziamentoPilota = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gridIncaricoPilRevoca = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridIncaricoPilRevoca)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // revocaIncPilBtn
+            // 
+            this.revocaIncPilBtn.Location = new System.Drawing.Point(1020, 636);
+            this.revocaIncPilBtn.Name = "revocaIncPilBtn";
+            this.revocaIncPilBtn.Size = new System.Drawing.Size(110, 23);
+            this.revocaIncPilBtn.TabIndex = 9;
+            this.revocaIncPilBtn.Text = "Revoca Incarico";
+            this.revocaIncPilBtn.UseVisualStyleBackColor = true;
+            this.revocaIncPilBtn.Click += new System.EventHandler(this.revocaIncPilBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 400);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Inserire data licenziamento:";
+            // 
+            // DataLicenziamentoPilota
+            // 
+            this.DataLicenziamentoPilota.Location = new System.Drawing.Point(25, 416);
+            this.DataLicenziamentoPilota.Name = "DataLicenziamentoPilota";
+            this.DataLicenziamentoPilota.Size = new System.Drawing.Size(200, 20);
+            this.DataLicenziamentoPilota.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Seleziona Incarico:";
+            // 
+            // gridIncaricoPilRevoca
+            // 
+            this.gridIncaricoPilRevoca.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridIncaricoPilRevoca.Location = new System.Drawing.Point(25, 30);
+            this.gridIncaricoPilRevoca.Name = "gridIncaricoPilRevoca";
+            this.gridIncaricoPilRevoca.Size = new System.Drawing.Size(534, 353);
+            this.gridIncaricoPilRevoca.TabIndex = 5;
+            this.gridIncaricoPilRevoca.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridIncaricoPilRevoca_CellContentClick);
+            // 
+            // FormRevocaIncarichiPiloti
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1155, 671);
+            this.Controls.Add(this.revocaIncPilBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DataLicenziamentoPilota);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridIncaricoPilRevoca);
+            this.Name = "FormRevocaIncarichiPiloti";
+            this.Text = "Revoca incarico pilota";
+            ((System.ComponentModel.ISupportInitialize)(this.gridIncaricoPilRevoca)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button revocaIncPilBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker DataLicenziamentoPilota;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gridIncaricoPilRevoca;
     }
 }
--- F1DBMS/FormRevocaIncarichiPiloti.cs
+++ F1DBMS/FormRevocaIncarichiPiloti.cs
@@ -10,11 +10,48 @@ using System.Windows.Forms;
 
 namespace F1DBMS
 {
-    public partial class Form1 : Form
+    public partial class FormRevocaIncarichiPiloti : Form
     {
-        public Form1()
+        private readonly DataClassesF1DataContext db;
+        private int tabellaIncarichiPilIndex;
+        public FormRevocaIncarichiPiloti(DataClassesF1DataContext originDb)
         {
             InitializeComponent();
+            db = originDb;
+            gridIncaricoPilRevoca.DataSource = from incPil in db.incarichi_pilotis
+                            select incPil;
+            tabellaIncarichiPilIndex = -1;
+        }
+
+        private void revocaIncPilBtn_Click(object sender, EventArgs e)
+        {
+            if(tabellaIncarichiPilIndex >= 0)
+            {
+                var incarico = (from incPil in db.incarichi_pilotis
+                                where incPil.CF.Equals(gridIncaricoPilRevoca.Rows[tabellaIncarichiPilIndex].Cells["CF"].Value)
+                                && incPil.dataAssunzione.Equals(gridIncaricoPilRevoca.Rows[tabellaIncarichiPilIndex].Cells["DataAssunzione"].Value)
+                                && !incPil.dataLicenziamento.HasValue
+                                && incPil.dataAssunzione <= DataLicenziamentoPilota.Value.Date
+                                select incPil).SingleOrDefault();
+                if(incarico != null)
+                {
+                    incarico.dataLicenziamento = DataLicenziamentoPilota.Value.Date;
+                    db.SubmitChanges();
+                    MessageBox.Show("Incarico revocato!");
+                    this.Close();
+                } else
+                {
+                    MessageBox.Show("Errore: ricontrolla la selezione o la data!");
+                }
+            } else
+            {
+                MessageBox.Show("Errore: ricontrolla la selezione o la data!");
+            }
+        }
+
+        private void gridIncaricoPilRevoca_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tabellaIncarichiPilIndex = e.RowIndex;
         }
     }
 }
