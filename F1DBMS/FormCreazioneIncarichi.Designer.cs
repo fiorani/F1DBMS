427974ab162ad444bcf72799822ef3d1602c7695
Mattia Panni <mattia.panni@studio.unibo.it>
2022-06-21 11:51:11 +0200

    Modify form creazione incarico dip

--- F1DBMS/FormCreazioneIncarichi.Designer.cs
+++ F1DBMS/FormCreazioneIncarichi.Designer.cs
@@ -85,7 +85,7 @@ namespace F1DBMS
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(608, 21);
+            this.label2.Location = new System.Drawing.Point(581, 21);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(110, 16);
             this.label2.TabIndex = 4;
