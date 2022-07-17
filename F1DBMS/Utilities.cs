bf8a900e4d260a70fa3073d7ab1a550436aead83
Mattia Panni <mattia.panni@studio.unibo.it>
2022-07-17 14:36:13 +0200

    Add utilities class

---
+++ F1DBMS/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace F1DBMS
+{
+    public static class Utilities
+    {
+        public static String assignValue(TextBox textBox)
+        {
+            return String.IsNullOrEmpty(textBox.Text) ? throw new Exception() : textBox.Text;
+        }
+    }
+}
